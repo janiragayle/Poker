b62afff8f39d83a4ab115b392bbc8aac07a851eb
Janira <janiragayle@gmail.com>
2024-04-06 18:09:04 +0100

    added method takeBankroll to player class + comments

--- Player.js
+++ Player.js
@@ -6,31 +6,35 @@ class Player {
         this.bet = 0;  
     }
 
-    addCard(c){
+    addCard(c){ //adds card c to player's hand
         this.hand.push(c);
     }
 
-    setCard(index, c){
+    setCard(index, c){ //sets the card at the given index to card c
         this.hand.splice(index, 0, c);
     }
     
-    removeCard(c){
+    removeCard(c){ // removes card c from player's hand
         this.hand.splice(c);
     }
 
-    bets(amt){
+    bets(amt){ //sets bet to players bet
         this.bet = amt;
     }
 
-    winnings(odds){
-        this.bankrolls += odds;
+    winnings(odds){ // adjusts player's winnings to bankroll
+        this.bankroll += odds;
     }
 
-    getBankroll(){
+    getBankroll(){ //returns balance of bankroll
         return this.bankroll;
     }
 
-    getBet(){
+    takeBankroll(b){ //
+        return this.bankroll -= b;
+    }
+
+    getBet(){ //returns player's bet
         return this.bet;
     }
 
